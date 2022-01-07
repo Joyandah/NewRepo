f38461b267ac5a1f1a886769b8989c84fe4aef1f
Joyandah <Joyandah@gmail.com>
2022-01-06 20:17:59 -0500

    new 1

--- NewrRepo/src/Test.java
+++ NewrRepo/src/Test.java
@@ -6,9 +6,12 @@ public class Test {
 		System.out.println("Hello world ");
 		System.out.println("This is the first text");
 		master("Morid");
-		
+		Sdeta(21);
 	}
 	public static void master(String name){
 	System.out.println(name);	
+	}
+	public static void Sdeta(int age) {
+		System.out.println(age);
 	}
 }
